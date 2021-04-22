8138f0901c40b37873a2e9ba4ce07c95f5b60314
Ashutosh Kumar <ashuisalive@gmail.com>
2021-04-22 23:59:56 +0530

    updated UI

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { Opening } from './components/animation/Opening';
 import { Blog } from './components/Blog/Blog';
 import { Contact } from './components/Contact/Contact';
 import { Navbar } from './components/Navbar/Navbar';
+import { Projects } from './components/Projects/Projects';
 
 function App() {
   return (
     <div>
       <Opening />
       <Navbar/>
-      <About/>
+      <About />
+      <Projects/>
       <Blog />
       <Contact/>
     </div>
--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import styles from "./ProjectCard.module.css";
 
-const ProjectCard = ({imageUrl, title, description, url}) => {
+const ProjectCard = ({ imgUrl, title, description, repoUrl, demoUrl, techStack }) => {
+    const openInNewTab = (url) => {
+        const newWindow = window.open(url, '__blank', 'noopener, norefferer')
+        if (newWindow) newWindow.opener = null;
+    }
     return (
-        <div>
-            
+        <div className={styles.blogCardWrapper}>
+            <img className={styles.img} src={imgUrl} alt="blog_logo" />
+            <p className={styles.title}>{title}</p>
+            <p className={styles.description}>{description}</p>
+            <div className={styles.techStack}>
+                {
+                    techStack.map(logo => <img src={logo} alt="logo"/>)
+                }
+            </div>
+            <div className={styles.btns}>
+                <button onClick={() => openInNewTab(demoUrl)}>DEMO</button>
+                <button onClick={() => openInNewTab(repoUrl)}>GITHUB</button>
+            </div>
         </div>
     )
 }
